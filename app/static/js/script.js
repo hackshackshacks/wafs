6aa2d7e3f08bdb504ac0d6e2469974e79fcfec2d
Koos <>
2018-02-07 13:37:11 +0100

    change to preventdefault

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -3,14 +3,17 @@
   const app = {
     init: function () {
       routes.init()
-      sections.toggle(helpers.getHash())
+      sections.toggle(window.location.hash)
+      document.querySelector('a').addEventListener((e) => {
+        e.preventdefault()
+      })
     }
   }
   /* Handle routes */
   const routes = {
     init: function () {
       window.addEventListener('hashchange', () => {
-        sections.toggle(helpers.getHash())
+        sections.toggle(window.location.hash)
       })
     }
   }
@@ -18,17 +21,11 @@
   const sections = {
     toggle: function (route) {
       const blocks = document.querySelectorAll('section')
-      let active = document.querySelector(`.${route}`)
+      let active = document.querySelector(route)
       blocks.forEach((block) => {
         block.classList.remove('active')
       })
       active.classList.add('active')
-    }
-  }
-  /* helper functions */
-  const helpers = {
-    getHash: function () {
-      return window.location.hash.split('#')[1]
     }
   }
   app.init()
